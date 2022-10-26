871709df72874a05f4634f0fe02269e7369fee23
c0deblack <79178809+c0deblack@users.noreply.github.com>
2022-10-25 20:16:31 -0400

    Changes

--- src/main/java/com/nof/vamathcalculator/FragmentHome.java
+++ src/main/java/com/nof/vamathcalculator/FragmentHome.java
@@ -8,6 +8,8 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.nof.vamathcalculator.databinding.FragmentHomeBinding;
@@ -25,18 +27,33 @@ public class FragmentHome extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         return binding.getRoot();
 
+        /**
+         * Add the DependencyStatus child fragment to the nested FragmentLayout within the
+         * fragment_home_scrolling.xml file.
+         */
+        FragmentManager fm = getChildFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+        ft.setReorderingAllowed(true);
+
+        ft.replace(R.id.home_sticky_content, DependencySummaryFragment.class, null);
+        ft.addToBackStack(null);
+
+
+        DisabilitySummaryScrollFragment ratings_frag = new DisabilitySummaryScrollFragment();
+        ft.replace(R.id.home_scroll_content, ratings_frag);
+        ft.addToBackStack(null);
+
+        CompensationSummaryFragment compensation_frag = new CompensationSummaryFragment();
+        ft.replace(R.id.home_compensation_content, compensation_frag);
+        ft.addToBackStack(null);
+
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        ft.commit();
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(FragmentHome.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
-            }
-        });
     }
 
     @Override
